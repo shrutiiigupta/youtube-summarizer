9db3c5dd8d26b96b32e4950f06594b509d322bf1
sanjeevani-25 <sanjeevani.lakade25@gmail.com>
2023-12-03 12:31:18 +0530

    .

--- pegasus_summarizer.py
+++ pegasus_summarizer.py
@@ -2,32 +2,34 @@ from transformers import PegasusForConditionalGeneration
 from transformers import PegasusTokenizer
 from transformers import pipeline
 
-def pegasus_summarizer():
-    # Pick model
-    model_name = "google/pegasus-xsum"
+# def pegasus_summarizer():
+# Pick model
+model_name = "google/pegasus-xsum"
 
-    # Load pretrained tokenizer
-    pegasus_tokenizer = PegasusTokenizer.from_pretrained(model_name)
+# Load pretrained tokenizer
+pegasus_tokenizer = PegasusTokenizer.from_pretrained(model_name)
 
-    with open('transcript.txt') as f:
-        example_text = f.read()
+example_text = open('transcript.txt').read()
 
-    # Define PEGASUS model
-    pegasus_model = PegasusForConditionalGeneration.from_pretrained(model_name)
+# Define PEGASUS model
+# pegasus_model = PegasusForConditionalGeneration.from_pretrained(model_name)
 
-    # Create tokens
-    tokens = pegasus_tokenizer(example_text, truncation=True, max_length=512, padding="max_length")
+# Create tokens
+# tokens = pegasus_tokenizer(example_text, truncation=True, max_length=7870, padding="max_length")
 
-    # Define summarization pipeline
-    summarizer = pipeline(
-        "summarization",
-        model=model_name,
-        tokenizer=pegasus_tokenizer,
-        framework="pt"
-    )
+# Define summarization pipeline
+summarizer = pipeline(
+    "summarization",
+    model=model_name,
+    tokenizer=pegasus_tokenizer
+)
+    # framework="pt"
 
-    summary = summarizer(example_text, min_length=30, max_length=50)
-    result=summary[0]["summary_text"]
-    # print(result)
-    return(result)
+# summary = summarizer(example_text,truncation=True)
+# summary = summarizer(example_text, min_length=200, max_length=1000,truncation=True)
+# summary = summarizer(example_text, min_length=200, max_length=1000)
+summary = summarizer(example_text)
+result=summary[0]["summary_text"]
+print(result)
+# return(result)
 
--- vid_to_text.py
+++ vid_to_text.py
@@ -1,26 +1,27 @@
+from re import A
 from youtube_transcript_api import YouTubeTranscriptApi as yta
 import audio_to_text
 import freq
 # import pegasus_summarizer
 import nlp_summarizer
-# import vid_id_extract
 import streamlit as st
 
-from nlp_summarizer import nlp_spacy_summarizer
 
 def vid_to_text(link,vid_id):
 
-    # harman singh - manual script - https://youtu.be/Wn9iALMyS7c?si=q0LYZBrR9mSbj2b7 
+    # hindi - harman singh - manual script - https://youtu.be/Wn9iALMyS7c?si=q0LYZBrR9mSbj2b7 
     # --- https://www.youtube.com/watch?v=Wn9iALMyS7c
 
-    # netflix death note dhruv rathee - no script allowed - https://www.youtube.com/watch?v=H3nzTmNlS4I&t=32s
-    # power couple - auto-generated only - https://www.youtube.com/watch?v=6dpF_G3yMMQ
-    # veryyyy long vid - 892 words transcript haha - https://www.youtube.com/watch?v=1qbna6t1bzw 
+    # hindi - netflix death note dhruv rathee - no script allowed - https://www.youtube.com/watch?v=H3nzTmNlS4I&t=32s
+    # eng - power couple - auto-generated only - https://www.youtube.com/watch?v=6dpF_G3yMMQ
+    # eng - manual(default) - veryyyy long vid - 892 words transcript haha - https://www.youtube.com/watch?v=1qbna6t1bzw 
     # what is  python - english - https://www.youtube.com/watch?v=Y8Tko2YC5hA
     # fraz google - https://www.youtube.com/watch?v=ZSoTKpKGkCU
     # tedx - https://www.youtube.com/watch?v=P6FORpg0KVo 
     # martin luther - https://youtu.be/_IB0i6bJIjw?si=2ZKZqi9uCfHC486K 
     # Ranveer Allahbadia - https://www.youtube.com/watch?v=3wBEUuV7BYg
+    # eng -manual script - 5 Fascinating Missions in Space | Planet Explorers: Full Series | BBC Earth Lab - https://www.youtube.com/watch?v=kvfa-6Pr36w
+    # en - mba speech - no transcript - https://www.youtube.com/watch?v=e0HlQh-hwyE
 
 
     # transcript_list = yta.list_transcripts(vid_id)
@@ -31,18 +32,22 @@ def vid_to_text(link,vid_id):
     # )
 
     try:
-        data = yta.get_transcript(vid_id)
+        data = yta.get_transcript(vid_id,languages=['en'])
         # print(data)
+        print("transcript try1 --> english only")
         return(clean_text(data))
     except:
         try:
-            data = yta.get_transcript(vid_id, languages=['en-IN','hi','mr', 'en'])
+            print("transcript try2 --> other languages")
+            data = yta.get_transcript(vid_id, languages=['en-IN','hi','mr'])
             return(clean_text(data))
         except:
-            # data = audio_to_text(link)
-            data="hi"
-
-        return(data)
+            print("transcript try3 --> ASR")
+            data = audio_to_text.audio_to_text(link)
+            if(data=='1'):
+                data=only_summary()
+            # data="hi"
+            return(data)
     
     # return(data)
 
@@ -62,7 +67,7 @@ def clean_text(data):
     # process_data = final_data.splitlines()
     # clean_data = ''.join(process_data)
     # print(clean_data)
-    text = open("transcript.txt",'w')
+    text = open("transcript.txt",'w',encoding='utf-8')
     text.write(final_data)
     text.close()
 
@@ -70,9 +75,24 @@ def clean_text(data):
     # summary_data=freq.main_freq()
 
     # method 2 --> spacy nlp summmarizer
+    # summary_data=nlp_summarizer.nlp_spacy_summarizer()
+
+    # summary_text = open("summary.txt",'w',encoding="utf-8")
+    # summary_text.write(summary_data)
+    # summary_text.close()
+
+    # return(final_data)
+    return(only_summary())
+
+def only_summary():
+
+    # method 1
+    # summary_data=freq.main_freq()
+
+    # method 2 --> spacy nlp summmarizer
     summary_data=nlp_summarizer.nlp_spacy_summarizer()
 
-    summary_text = open("summary.txt",'w')
+    summary_text = open("summary.txt",'w',encoding="utf-8")
     summary_text.write(summary_data)
     summary_text.close()
 
