27c3e95998019b8664bdf8d8b020000020dd7bb0
sanjeevani-25 <sanjeevani.lakade25@gmail.com>
2023-12-05 01:38:40 +0530

    finn

--- method4_nltk_and_pegasus/m2_pegasus.py
+++ method4_nltk_and_pegasus/m2_pegasus.py
@@ -2,15 +2,15 @@ from transformers import PegasusForConditionalGeneration
 from transformers import PegasusTokenizer
 from transformers import pipeline
 
-def pegasus_summarizer():
+def pegasus():
     # Pick model
     model_name = "google/pegasus-xsum"
 
     # Load pretrained tokenizer
     pegasus_tokenizer = PegasusTokenizer.from_pretrained(model_name)
 
-    example_text = open('./transcript.txt').read()
-    print(example_text)
+    example_text = open('./summary_temp.txt','r',encoding="utf8").read()
+    # print(example_text)
 
     # Define summarization pipeline
     summarizer = pipeline(
@@ -20,14 +20,18 @@ def pegasus_summarizer():
         framework="pt"
     )
 
-    summary = summarizer(example_text, min_length=100, max_length=150)
+    # summary = summarizer(example_text, min_length=100, max_length=150)
+    # summary = summarizer(example_text)
+    summary = summarizer(example_text, min_length=100, max_length=512,truncation=True)
+
     result = summary[0]["summary_text"]
 
     # Write the result to the output file
-    with open("./result.txt", "w") as result_file:
-        result_file.write(result)
+    # with open("./summary.txt", "w") as result_file:
+    #     result_file.write(result)
 
-    print("Result has been saved to result.txt")
-    print(result)
+    print("Result has been saved to summary.txt")
+    # print(result)
+    return(result)
 
-# pegasus_summarizer()
+# pegasus()
