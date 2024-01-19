91213cfe9e0e9e9cb00fcf7676c9cfdf214dc3a7
sanjeevani-25 <sanjeevani.lakade25@gmail.com>
2024-01-19 23:34:05 +0530

    rr

--- metrics_eval/rouge_score_1.py
+++ metrics_eval/rouge_score_1.py
@@ -10,7 +10,7 @@ def rouge_func():
     # metrics=['rouge-n', 'rouge-l', 'rouge-w']
     metrics=['rouge-n', 'rouge-l']
     # metrics=['rouge-1','rouge-2', 'rouge-l']
-    rouge2 = rouge.Rouge(metrics,max_n=2)
+    rouge2 = rouge.Rouge(metrics,2)
 
     scores = rouge2.get_scores(generated_summary, reference_summary)
 
